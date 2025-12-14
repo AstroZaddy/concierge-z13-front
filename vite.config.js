22799f33811abd96403d378c9cb51888eddcf1b5
AstroZaddy <ardubs@proton.me>
2025-12-14 15:14:16 -0600

    .

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-
 export default defineConfig({
-  server: {
-    host: "127.0.0.1",
-    port: 4322,
-    allowedHosts: [
-      "z13astrology.com",
-      "www.z13astrology.com",
-    ],
-  },
-});
+    vite: {
+      server: {
+        allowedHosts: true,
+      },
+    },
+  });
